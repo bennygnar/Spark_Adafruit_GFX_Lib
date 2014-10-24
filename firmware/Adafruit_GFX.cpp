c8b1640d35ec2b887fb6ca0558a4da89b6709f3d
Ben Reese <reeseb@gmail.com>
2014-10-23 22:02:19 -0600

    Changed name back

--- firmware/Adafruit_GFX.cpp
+++ firmware/Adafruit_GFX.cpp
@@ -31,7 +31,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "Adafruit_GFX/Adafruit_GFX.h"
+#include "Adafruit_GFX.h"
 
 #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
 
